a34bd97755b4cb8565a88dbdaf161d1ceebd455b
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-09-13 08:01:46 +0300

    Update on HTML, CSS, and JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,31 @@
+
+
 function init() {
-    console.log('hello');
-    
+    console.log('hello')
     const cvURL = document.querySelector('.resume')
+    const projectURL = document.querySelectorAll('.projectLink')
+    const projectName = document.querySelectorAll('.projectTitle')
 
     function resumeBtnClick() {
         window.open("")
     }
     cvURL.addEventListener('click', resumeBtnClick)
+
+    function gitHubClick() {
+        for (let n = 0; n < projectName.length; n++) {
+            const name = projectName[n].textContent.replaceAll(' ', '-')            
+            if ((name !== '') && (name === this.id)) {
+                window.open(`https://github.com/iijumanaAhmed/${this.id}`)
+                break
+            }
+        }
+    }
+
+    for (let i = 0; i < projectURL.length; i++) {
+        if (projectURL[i] !== '') {
+            projectURL[i].addEventListener('click', gitHubClick)
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init)
