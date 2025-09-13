a3b816668a421107314d8fbcd8b13e3255af9fc1
Jumana Ahmed Khawaji <jumanaAhmedCS@outlook.com>
2025-09-13 10:24:49 +0300

    Updates on HTML, CSS, and JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
-
-
 function init() {
-    console.log('hello')
-    const cvURL = document.querySelector('.resume')
+    const cvURL = document.querySelector('.btn')
     const projectURL = document.querySelectorAll('.projectLink')
     const projectName = document.querySelectorAll('.projectTitle')
+    const socialAccount = document.querySelectorAll('.account')
+    const senderName = document.querySelector('.fullNameInput')
+    const senderEmail = document.querySelector('.emailInput')
+    const senderMeassage = document.querySelector('.meassageInput')
+    const formBtn = document.querySelector('#sendForm')
 
     function resumeBtnClick() {
         window.open("")
@@ -13,7 +15,7 @@ function init() {
 
     function gitHubClick() {
         for (let n = 0; n < projectName.length; n++) {
-            const name = projectName[n].textContent.replaceAll(' ', '-')            
+            const name = projectName[n].textContent.replaceAll(' ', '-')
             if ((name !== '') && (name === this.id)) {
                 window.open(`https://github.com/iijumanaAhmed/${this.id}`)
                 break
@@ -26,6 +28,29 @@ function init() {
             projectURL[i].addEventListener('click', gitHubClick)
         }
     }
+
+    function accountClick() {
+        if (this.textContent === 'Email') {
+            window.open('mailto:JumanaAhmedCS@outlook.com')
+        } else if (this.textContent === 'GitHub') {
+            window.open('https://github.com/iijumanaAhmed')
+        } else if (this.textContent === 'LinkedIn') {
+            window.open('https://www.linkedin.com/in/jumana-khawaji-0488382b8')
+        }
+    }
+
+    for (let i = 0; i < socialAccount.length; i++) {
+        socialAccount[i].addEventListener('click', accountClick)
+    }
+
+    function formValidation() {
+        document.querySelector('.contactForm').action = 'https://submit-form.com/u51dADX3t'
+    }
+    formBtn.addEventListener('click', formValidation)
+
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init)
